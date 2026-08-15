d9a34c5cf07bb4cf4781bb438aea481a3a3accf4
ShreyaDebnath31 <moonsdebnath@gmail.com>
2026-08-15 23:59:33 +0530

    Create basic student portal menu

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Student Portal ===\n";
+    cout << "1. Show Students\n";
+    cout << "2. Add Student\n";
+    cout << "3. Search Student\n";
+    cout << "4. Exit\n";
+
+    return 0;
+}
