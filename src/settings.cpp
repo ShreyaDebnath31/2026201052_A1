89cb7cee3a6401f22e877af65083d1106ec76d4c
ShreyaDebnath31 <moonsdebnath@gmail.com>
2026-08-16 11:40:02 +0530

    Add notification preference setting

--- src/settings.cpp
+++ src/settings.cpp
@@ -4,3 +4,11 @@ using namespace std;
 void showSettings() {
     cout << "\n--- Student Portal Settings ---\n";
 }
+
+void SetNotifications(bool enabled){
+    if(enabled){
+        cout<<" Notifications enabled"<<endl;
+    }else{
+        cout<<"Notifications disabled"<<endl;
+    }
+}
