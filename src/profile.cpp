55ad4111f74e4fe64323ebbd4227df9862d73081
ShreyaDebnath31 <moonsdebnath@gmail.com>
2026-08-16 11:21:43 +0530

    Add student profile display

---
+++ src/profile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void showProfile(string name, string course) {
+    cout << "\n--- Student Profile ---\n";
+    cout << "Name: " << name << '\n';
+    cout << "Course: " << course << '\n';
+}
