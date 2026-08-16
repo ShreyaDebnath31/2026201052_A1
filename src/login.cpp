781f895c93b37813aac0bc87e8a6b12f63fa3775
ShreyaDebnath31 <moonsdebnath@gmail.com>
2026-08-16 11:11:36 +0530

    Add interactive student login

--- src/login.cpp
+++ src/login.cpp
@@ -5,3 +5,20 @@ using namespace std;
 bool loginUser(string username, string password) {
     return username == "student" && password == "1234";
 }
+void startLogin() {
+    string username;
+    string password;
+
+    cout << "Username: ";
+    cin >> username;
+
+    cout << "Password: ";
+    cin >> password;
+
+    if (loginUser(username, password)) {
+        cout << "Login successful.\n";
+    }
+    else {
+        cout << "Invalid username or password.\n";
+    }
+}
