39b7be77160c5db2e9a702fc9d71e5ccf1718659
ShreyaDebnath31 <moonsdebnath@gmail.com>
2026-08-16 11:13:07 +0530

    Limit set for student login attempts

--- src/login.cpp
+++ src/login.cpp
@@ -5,20 +5,25 @@ using namespace std;
 bool loginUser(string username, string password) {
     return username == "student" && password == "1234";
 }
+
 void startLogin() {
     string username;
     string password;
 
-    cout << "Username: ";
-    cin >> username;
+    for (int attempt = 1; attempt <= 3; attempt++) {
+        cout << "Username: ";
+        cin >> username;
 
-    cout << "Password: ";
-    cin >> password;
+        cout << "Password: ";
+        cin >> password;
 
-    if (loginUser(username, password)) {
-        cout << "Login successful.\n";
-    }
-    else {
+        if (loginUser(username, password)) {
+            cout << "Login successful.\n";
+            return;
+        }
+
         cout << "Invalid username or password.\n";
     }
+
+    cout << "Too many failed login attempts.\n";
 }
