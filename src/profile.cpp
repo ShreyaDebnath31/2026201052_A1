f9d953db6626f9cb484f9c59ddee2c686cc2061b
ShreyaDebnath31 <moonsdebnath@gmail.com>
2026-08-16 11:23:09 +0530

    Display student profile identifier

--- src/profile.cpp
+++ src/profile.cpp
@@ -2,8 +2,10 @@
 #include <string>
 using namespace std;
 
-void showProfile(string name, string course) {
+void showProfile(int id,string name, string course) {
     cout << "\n--- Student Profile ---\n";
+    cout << "Student ID: "<<id<<endl;
     cout << "Name: " << name << '\n';
     cout << "Course: " << course << '\n';
 }
+
